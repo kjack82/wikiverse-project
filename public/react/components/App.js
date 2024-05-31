2c22709dd49c46e91d941673a3c3a8132591ebec
kjack82 <kprothero1702@gmail.com>
2024-05-31 15:47:14 +0100

    project coding from Kieran

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,31 +1,112 @@
-import React, { useState, useEffect } from 'react'
-import { PagesList } from './PagesList'
+import React, { useEffect, useState } from 'react'
+import { Page } from './Page'
+import { Form } from './Form'
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api'
 
 export const App = () => {
   const [pages, setPages] = useState([])
+  const [activePage, setActivePage] = useState(null)
+  const [isAddingPage, setIsAddingPage] = useState(false)
 
-  async function fetchPages () {
-    try {
-      const response = await fetch(`${apiURL}/wiki`)
-      const pagesData = await response.json()
-      setPages(pagesData)
-    } catch (err) {
-      console.log('Oh no an error! ', err)
+  function goHome() {
+    setActivePage(null)
+    setIsAddingPage(false)
+  }
+
+  async function getPage(slug) {
+    const response = await fetch(`${apiURL}/wiki/${slug}`)
+    const pageData = await response.json()
+    setActivePage(pageData)
+  }
+
+  async function addPage(page) {
+    const response = await fetch(`${apiURL}/wiki`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(page)
+    })
+
+    const newPage = await response.json()
+    setPages([...pages, newPage])
+    setIsAddingPage(false)
+  }
+
+  async function deletePage(slug) {
+    const response = await fetch(`${apiURL}/wiki/${slug}`, {
+      method: 'DELETE'
+    })
+
+    if (response.ok) {
+      const filteredPages = pages.filter((page) => page.slug !== slug)
+      setPages(filteredPages)
+      setActivePage(null)
+    } else {
+      console.log(response.status + ' ' + response.statusText)
     }
   }
 
   useEffect(() => {
+    async function fetchPages() {
+      try {
+        const response = await fetch(`${apiURL}/wiki`)
+        const pagesData = await response.json()
+        setPages(pagesData)
+      } catch (err) {
+        console.log('Oh no an error! ', err)
+      }
+    }
+
     fetchPages()
   }, [])
 
+  useEffect(() => {
+    if (activePage) {
+      document.title = `${activePage.title} - WikiVerse`
+    } else if (isAddingPage) {
+      document.title = 'Add a Page - WikiVerse'
+    } else {
+      document.title = 'WikiVerse'
+    }
+  }, [activePage, isAddingPage])
+
+  if (isAddingPage) {
+    return <Form goHome={goHome} addPage={addPage} />
+  }
+
+  if (activePage) {
+    return <Page {...activePage} goHome={goHome} deletePage={deletePage} />
+  }
+
   return (
-		<main>
-      <h1>WikiVerse</h1>
-			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
-		</main>
+    <main>
+      <h1 style={{ marginBottom: 0 }}>WikiVerse</h1>
+      <p style={{ marginTop: 0 }}>An interesting 📚</p>
+      <ul>
+        {pages.map((page) => (
+          <li key={page.id}>
+            <button
+              className="link"
+              type="button"
+              onClick={() => getPage(page.slug)}
+            >
+              {page.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <p>
+        <button
+          className="link"
+          type="button"
+          onClick={() => setIsAddingPage(true)}
+        >
+          Add a Page &rarr;
+        </button>
+      </p>
+    </main>
   )
 }
--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,7 +1,47 @@
 import React from 'react'
 
 export const Page = (props) => {
-  return <>
-    <h3>{props.page.title}</h3>
-  </>
+  const deletePage = () => {
+    const isConfirmed = window.confirm(
+      'Are you sure you want to delete this page?'
+    )
+
+    if (isConfirmed) {
+      props.deletePage(props.slug)
+    }
+  }
+
+  return (
+    <>
+      <h1>{props.title}</h1>
+      <p>
+        <b>Author:</b> {props.author.name}
+      </p>
+      <p>
+        <b>Published:</b>{' '}
+        <time dateTime={props.createdAt}>
+          {new Date(props.createdAt).toLocaleDateString()}
+        </time>
+      </p>
+      <p>{props.content}</p>
+      <p>
+        <b>Tags:</b>
+      </p>
+      <ul>
+        {props.tags.map((tag) => (
+          <li key={tag.id}>{tag.name}</li>
+        ))}
+      </ul>
+      <p>
+        <button type="button" onClick={deletePage}>
+          Delete Page
+        </button>
+      </p>
+      <p>
+        <button className="link" type="button" onClick={props.goHome}>
+          &larr; Back to Wiki List
+        </button>
+      </p>
+    </>
+  )
 }
